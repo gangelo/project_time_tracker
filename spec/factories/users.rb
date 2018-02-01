4d91741bd4a533e45b973c120fd5b5a820525699
gangelo <web.gma@gmail.com>
2018-02-01 13:05:02 -0500

    Set up initial User model tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    password '!Passw0rd'
+    confirmation_token { '0123456789' }
+    confirmation_sent_at { Time.now.utc }
+    confirmed_at { Time.now.utc }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "factories" do
+    describe "#user" do
+      subject { FactoryBot.build(:user) }
+      it { should be_valid }
+    end
+  end
+
+  context "validations" do
+    describe "#email" do
+      it { should validate_length_of(:email).is_at_least(3).with_message("must be between 3 and 254 characters") }
+      it { should validate_length_of(:email).is_at_most(254).with_message("must be between 3 and 254 characters") }
+      it { should_not allow_value("something.at.somewhere.com").for(:email) }
+      it { should allow_value("something@somewhere.com").for(:email) }
+
+      it "should be unique, case-insensitive" do
+        User.create(email: "user_1@gmail.com", password: "password")
+        non_unique_user = User.create(email: "user_1@gmail.com", password: "password")
+        expect(non_unique_user).not_to be_valid
+      end
+    end
+  end
 end
