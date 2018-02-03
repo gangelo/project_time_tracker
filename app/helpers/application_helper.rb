f98c83f8ae805bb1dce3ab5354fbb994e0d0e171
gangelo <web.gma@gmail.com>
2018-02-03 12:27:33 -0500

    Add authenticated? alias for current_user?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
- # Returns the authenticated user if one is logged in, or a blank new user if not.
- #
- def resource
-   current_user || User.new
- end
+  # Returns the authenticated user if one is logged in, or a blank new user if not.
+  def resource
+    current_user || User.new
+  end
 
- def current_user?
-   current_user
- end
+  def current_user?
+    current_user
+  end
+
+  alias authenticated? current_user?
 end
