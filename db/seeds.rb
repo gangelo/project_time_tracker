29648da531659cd9ce040840c855b21de8c31024
gangelo <web.gma@gmail.com>
2018-02-05 12:20:30 -0500

    Add seed data specific to environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Role.create!({ name: 'user' })
-Role.create!({ name: 'admin' })
+@total_users = 0
+@admin_role = nil
+@user_role = nil
 
-(0...200).each do |i|
-  email = i == 0 ? "admin@gmail.com" : "user#{i}@gmail.com"
-  user = User.create({ email: email, password: "password" })
-  user.confirmed_at = DateTime.now
-  user.save!
+# Roles
+def create_roles
+  @admin_role = Role.create!(name: 'admin') if @admin_role.nil?
+  @user_role = Role.create!(name: 'user') if @user_role.nil?
+end
+
+# Admin
+def create_admin
+  admin = User.new(email: "admin@gmail.com", password: "password")
+  admin.confirmed_at = DateTime.now
+  admin.roles << @user_role
+  admin.roles << @admin_role
+  admin.save!
+end
+
+# Users
+case Rails.env
+when 'development'
+  total_users = 150
+when 'test'
+  total_users = 10
+else
+  total_users = 0
+end
+
+if total_users > 0
+  ActiveRecord::Base.transaction do
+    create_roles
+    create_admin
+    (0...10).each do |i|
+      user = User.new(email: "user#{i}@gmail.com", password: "password")
+      user.roles << @user_role
+      user.confirmed_at = DateTime.now
+      user.save!
+    end
+  end
 end
