866452c6fda7f85b09a24d115c30dea6a2088167
gangelo <web.gma@gmail.com>
2018-02-09 11:11:31 -0500

    Add project, and company modles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209160700) do
+ActiveRecord::Schema.define(version: 20180209161040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_companies_on_project_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.bigint "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_projects_on_task_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
@@ -60,6 +76,8 @@ ActiveRecord::Schema.define(version: 20180209160700) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "projects"
+  add_foreign_key "projects", "tasks"
   add_foreign_key "user_roles", "roles"
   add_foreign_key "user_roles", "users"
 end
