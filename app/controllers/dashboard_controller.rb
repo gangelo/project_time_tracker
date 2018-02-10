c60200af614cd2045faba531a951484c03aa1d6a
gangelo <web.gma@gmail.com>
2018-02-10 15:23:32 -0500

    Move some code in DashboardController to helper module

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  include DashboardHelper
+  
   before_action :authenticate_user!
 
   def index
@@ -160,23 +162,6 @@ class DashboardController < ApplicationController
   end
 
   protected
-
-  def task_time_for_current_user(task_time_id)
-    task_time = TaskTime.find(task_time_id)
-    return task_time unless task_time.nil? || task_time.user_id != current_user.id
-    yield "The Task Time is not associated with you"
-  rescue ActiveRecord::RecordNotFound
-    yield "The Task Time could not be found"
-  end
-
-  def force_task_stop(task_times)
-    start_time = task_times.start_time
-    duration = task_times.duration || 0
-    duration += ((DateTime.now - DateTime.parse(start_time.to_s(:rfc822))) * 24 * 60 * 60).to_i
-    task_times.duration = duration
-    task_times.start_time = nil
-    task_times.save
-  end
 
   def create_task_time_params
     params.permit(:companies, :projects, :tasks)
